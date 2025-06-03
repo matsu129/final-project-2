1f01f3b5f49304afe9b2659b03f17141ee5a99a1
matsu129 <78124432+matsu129@users.noreply.github.com>
2025-06-02 19:31:46 -0700

    add imgs and script.js edit index.html and style.css

---
+++ script.js
@@ -0,0 +1,45 @@
+// dvd slider
+const images = [
+  "./imgs/DVD_01.png",
+  "./imgs/DVD_02.png",
+  "./imgs/DVD_03.png",
+  "./imgs/DVD_04.png"
+];
+let currentIndex = 0;
+
+const imgElement = document.getElementById("dvd-img");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+
+function updateImage() {
+  imgElement.setAttribute("src", images[currentIndex]);
+}
+
+prevButton.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  updateImage();
+});
+
+nextButton.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateImage();
+});
+
+
+// back to top
+const backToTopButton = document.getElementById("back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
